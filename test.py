46804d595dd8fd5615e056d53c26d267874944d5
wangdapao666 <583087864@qq.com>
2022-08-21 22:05:15 +0800

    test commit for changed  file

---
+++ test.py
@@ -0,0 +1,9 @@
+'''
+Author: wangdapao666 583087864@qq.com
+Date: 2022-08-21 22:02:10
+LastEditors: wangdapao666 583087864@qq.com
+LastEditTime: 2022-08-21 22:04:15
+FilePath: \undefinedd:\Git\Test\test.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+print("hello world")
